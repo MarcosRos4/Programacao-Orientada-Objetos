c9ded22ed91ed9d5b9e6217bd16cac551a6105f2
Marcos Vinicius Cavalcante Rosa <103190249+MarcosRos4@users.noreply.github.com>
2023-03-07 18:43:31 -0300

    agora funciona de verdade

--- Atividade de Reforco/AppContaCorrente.java
+++ Atividade de Reforco/AppContaCorrente.java
@@ -1,5 +1,5 @@
+package contacorrentepackage;
 import java.util.Scanner;
-
 
 public class AppContaCorrente{
 
@@ -14,67 +14,66 @@ public class AppContaCorrente{
     corrente1.setsaldoConta(12355);
     
     Agencia agencia2 = new Agencia();
-    agencia1.setidAgencia("0987");
-    agencia1.setnomeAgencia("Agencia do Thomas");
+    agencia2.setidAgencia("0987");
+    agencia2.setnomeAgencia("Agencia do Thomas");
 
     ContaCorrente corrente2 = new ContaCorrente();
-    corrente1.setnumeroConta("4567");
-    corrente1.setsaldoConta(1238955);
+    corrente2.setnumeroConta("4567");
+    corrente2.setsaldoConta(1238955);
     
-
     ContaCorrente[] contas = {corrente1, corrente2};
 
     Agencia[] agencias = {agencia1, agencia2};
 
+    String a; String c; String o="f";
 
+    try (Scanner sc = new Scanner(System.in)) {
+        
+        do {
 
-    Scanner sc = new Scanner(System.in);
-    String a; String c; String o="f"; 
-    do {
+            System.out.print("Informe o Número da Agência e da Conta:\nAgência: ");
 
-        System.out.print("Informe o Número da Agência e da Conta:\nAgência: ");
-
-        a = sc.next();
-        System.out.print("Conta: ");
-        c = sc.next();
+            a = sc.next();
+            System.out.print("Conta: ");
+            c = sc.next();
+            
+        } while (procurar(a, c, agencias, contas) == false);
         
-    } while (procurar(a, c, agencias, contas) == false);
-    
-    do {
-        System.out.print("Informe a próxima ação desejada:\nConsulta de Saldo = C\nDepósito = D\nSaque = S\nEncerrar = E\nOpção: ");
-    o = sc.next();
-    o.toLowerCase();
-    switch (o) {
-        case "c":
-            consulta(c, contas);
-            break;
-        
-        case "d":
-            System.out.print("Informe um valor a ser Depositado: ");
-            float v = sc.nextFloat();
-            deposito(c, contas, v);
-            break;
-        
-        case "s":
-            System.out.print("Informe um valor a ser Sacado: ");
-            float s = sc.nextFloat();
-            saque(c, contas, s);
-            break;
-        
-        case "e":
-            System.out.println("Programa Encerrado.");
-            break;
-        default:
-            System.out.println("Opção inválida");
-            break;
+        do {
+            System.out.print("Informe a próxima ação desejada:\nConsulta de Saldo = C\nDepósito = D\nSaque = S\nEncerrar = E\nOpção: ");
+            o = sc.next().toLowerCase();
+            switch (o) {
+                case "c":
+                    consulta(c, contas);
+                    break;
+                
+                case "d":
+                    System.out.print("Informe um valor a ser Depositado: ");
+                    float v = sc.nextFloat();
+                    deposito(c, contas, v);
+                    break;
+                
+                case "s":
+                    System.out.print("Informe um valor a ser Sacado: ");
+                    float s = sc.nextFloat();
+                    saque(c, contas, s);
+                    break;
+                
+                case "e":
+                    System.out.println("Programa Encerrado.");
+                    break;
+                default:
+                    System.out.println("Opção inválida");
+                    break;
+            }
+        } while (o.equals("e") == false);
     }
-    } while (o != "e");
-    }
+}
 
     public static boolean procurar(String ag, String co, Agencia[] obj, ContaCorrente[] abj) {
+
         for (int i = 0; i < 2; i++) {
-        
-            if (obj[i].getidAgencia().equals(ag) == abj[i].getnumeroConta().equals(co)) {
+            if (obj[i].getidAgencia().equals(ag) && abj[i].getnumeroConta().equals(co)) {
                 System.out.println("Dados Encontrados!");
                 return true;
             }
